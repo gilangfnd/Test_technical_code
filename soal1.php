059c1654c8ab3fc60618a210feea4c7bd3b90f83
Gilang Fandi Setiaji <gilangfnd@gmail.com>
2023-03-03 19:51:58 +0700

    first commit

--- soal1.php
+++ soal1.php
@@ -1,3 +1,53 @@
-<?php
+<!DOCTYPE HTML>  
+<html>
+<head>
+</head>
+<body>  
 
-<Form Name ="form1" Method ="POST" ACTION = "basicForm.php">
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["Input Number"])) {
+    $numberErrr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }
+
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>PHP Form Validation Example</h2>
+<form 
+  method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+    Number: <input type="text" name="name" value="<?php echo $name;?>">
+    <span class="error">* <?php echo $nameErr;?></span>
+    <br><br>
+  
+
+  <button>Generate segitiga</button>
+  <button>Generate Bilangan Ganjil</button>
+  <button>Generate Bilangan Prima</button>
+</form>
+
+<?php
+echo "<h2>Result:</h2>";
+echo $name;
+
+?>
+
+</body>
+</html>
